6de5ca39b746f009ec704d7e2effce6729f5bb4e
ljr <488313803@qq.com>
2022-12-20 11:29:46 +0800

    初始化

---
+++ src/main/java/ings/vip/mybatisplus/IngsApplication.java
@@ -0,0 +1,15 @@
+package ings.vip.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("ings.vip.mybatisplus.*")
+@SpringBootApplication
+public class IngsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ings/vip/mybatisplus/table/TestLabel.java
@@ -0,0 +1,23 @@
+package ings.vip.mybatisplus.table;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @author ljr
+ * @date 2022-09-23 10:13
+ */
+@Data
+@TableName("test_label")
+public class TestLabel {
+    private int id;
+    private String name;
+
+   @TableField(exist = false)
+    private String label;
+
+    public static int A() {
+        return 0;
+    }
+}
